7bd45113ab7cbd54770e26721c602230e1c9b357
ketan <ketan@LAPTOP-R3MOBSF6>
2022-07-18 11:31:14 +0530

    hi

---
+++ src/test/java/login/testcase5.java
@@ -0,0 +1,13 @@
+package login;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class testcase5 {
+
+	@Test
+	
+	public void tc5() {
+		Reporter.log("automation job 7-8l package",true);
+	}
+}
---
+++ src/test/java/login/testcase6.java
@@ -0,0 +1,13 @@
+package login;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class testcase6 {
+
+	@Test
+	
+	public void tc8() {
+		Reporter.log("great work",true);
+	}
+}
